3ae65ebba98e29e2e58758c360bb84f284ccf84f
Christian Sitjà <101610337+blackcat80@users.noreply.github.com>
2023-04-29 16:19:29 +0200

    Add files via upload

---
+++ push2/src/swap.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csitja-b <csitja-b@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 19:44:47 by csitja-b          #+#    #+#             */
+/*   Updated: 2023/04/28 19:57:59 by csitja-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* swap:
+*	Swaps the top 2 elements of a stack.
+*	Does nothing if there is only one or no elements.
+*/
+static void	swap(t_stack *stack)
+{
+	int	tmp;
+
+	if (stack == NULL || stack->next == NULL)
+		return ;
+	tmp = stack->value;
+	stack->value = stack->next->value;
+	stack->next->value = tmp;
+	tmp = stack->index;
+	stack->index = stack->next->index;
+	stack->next->index = tmp;
+}
+
+/* do_sa:
+*	Swaps the top 2 elements of stack a.
+*	Prints "sa" to the standard output.
+*/
+void	do_sa(t_stack **stack_a)
+{
+	swap(*stack_a);
+	ft_putstr("sa\n");
+}
+
+/* do_sb:
+*	Swaps the top 2 elements of stack b.
+*	Prints "sb" to the standard output.
+*/
+void	do_sb(t_stack **stack_b)
+{
+	swap(*stack_b);
+	ft_putstr("sb\n");
+}
+
+/* do_ss:
+*	Swaps the top 2 elements of stack a and the top 2 elements
+*	of stack b.
+*	Prints "ss" to the standard output.
+*/
+void	do_ss(t_stack **stack_a, t_stack **stack_b)
+{
+	swap(*stack_a);
+	swap(*stack_b);
+	ft_putstr("ss\n");
+}
