51f59a06f7230545e7e78e7318eb0e105f14b343
Christian SB <csitja80@gmail.com>
2023-07-11 23:55:27 +0200

    added print_stack function just to check

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: csitja-b <csitja-b@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 19:43:30 by csitja-b          #+#    #+#             */
-/*   Updated: 2023/07/11 18:15:49 by csitja-b         ###   ########.fr       */
+/*   Updated: 2023/07/11 23:53:16 by csitja-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,16 @@ static void	push_swap(t_stack **stack_a, t_stack **stack_b, int stack_size)
 		sort(stack_a, stack_b);
 }
 
+static void print_stack(t_stack *stack)
+{
+    while (stack)
+    {
+        printf("%d ", stack->value); // Imprime el valor del elemento
+        stack = stack->next; // Avanza al siguiente nodo del stack
+    }
+    printf("\n"); // Imprime una nueva línea al final para una mejor legibilidad
+}
+
 /* main:
 	Comprueba si la entrada es correcta, en cuyo caso 
 	inicializa las pilas a y b,	asigna cada índice de valor y 
@@ -62,6 +72,7 @@ int	main(int ac, char **av)
 	stack_size = get_stack_size(stack_a);
 	assign_index(stack_a, stack_size + 1);
 	push_swap(&stack_a, &stack_b, stack_size);
+	print_stack(stack_a);
 	free_stack(&stack_a);
 	free_stack(&stack_b);
 	return (0);
